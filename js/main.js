246e0bff188efd0136931b13b812c51f04c9b1b9
ctrl-nobody <par.12092000@gmail.com>
2021-01-12 05:15:06 +0300

    create: modal window + validation forms

--- js/main.js
+++ js/main.js
@@ -8,10 +8,31 @@ $('.header__mobile-menu').click(function(){
     $('body').toggleClass('body-hidden');
 })
 
-$('.show-me-window').click(function(){
-    $('.hidden-window').addClass('visible-window');
-    $('body').toggleClass('body-hidden-two');
-})
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $('.modal__close');
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+function openModal(){
+  var modalOverlay=$('.modal__overlay');
+  var modalDialog=$('.modal__dialog');
+  modalOverlay.addClass('modal__overlay--visible')
+  modalDialog.addClass('modal__dialog--visible')
+}
+$(document).keyup(function(e) {
+  if (e.key === "Escape") {
+    var modalOverlay=$('.modal__overlay');
+    var modalDialog=$('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible')
+    modalDialog.removeClass('modal__dialog--visible')
+  }
+});
+function closeModal(event){
+  event.preventDefault()
+  var modalOverlay=$('.modal__overlay');
+  var modalDialog=$('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible')
+  modalDialog.removeClass('modal__dialog--visible')
+}
 
 var mySwiperOne = new Swiper('.swiper-container-reviews', {
     // Optional parameters
@@ -24,3 +45,51 @@ var mySwiperOne = new Swiper('.swiper-container-reviews', {
     },
 })
 
+$(document).ready(function(){
+  $('.form-validation').each( function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please, specify your name",
+          minlength: "At least 2 letters please!"
+        },
+        email: {
+          required: "Your email is required",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please, specify your phone",
+          minlength: "At least 10 numbers please!"
+        },
+      },
+    });
+  });
+
+  $('.form-validation-another').each( function() {
+    $(this).validate({
+      errorClass: "invalid-messages",
+      messages: {
+        name: {
+          required: "Please, specify your name",
+          minlength: "At least 2 letters please!"
+        },
+        email: {
+          required: "Your email is required",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please, specify your phone",
+          minlength: "At least 10 numbers please!"
+        },
+      },
+      errorPlacement: function(label, element) {
+        label.addClass('arrow');
+        label.insertAfter(element);
+      },
+      wrapper: 'span'
+    });
+  });
+  $('.form-phone').mask('+7(000) 000-00-00');
+  AOS.init();
+})
